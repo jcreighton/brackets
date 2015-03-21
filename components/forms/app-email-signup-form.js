fe650b9ac3c5f285c144a3e93ba501e5de6f8033
Jenn Creighton <jenn@chartbeat.com>
2015-03-21 15:45:59 -0400

    fix firebase URL for email list

--- components/forms/app-email-signup-form.js
+++ components/forms/app-email-signup-form.js
@@ -1,7 +1,7 @@
 /** @jsx React.DOM */
 var React = require('react');
 var Firebase = require('firebase');
-var EmailListRef = new Firebase('https://glowing-inferno-6073.firebaseio.com');
+var EmailListRef = new Firebase('https://glowing-inferno-6073.firebaseio.com/email_list');
 var Email = require('../inputs/app-email.js');
 var TextBox = require('../inputs/app-textbox.js');
 var Submit = require('../buttons/app-event-button.js');
