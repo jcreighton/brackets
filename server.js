dbb131580187ad2b2df32e932076879b383b245c
Jenn Creighton <jenn@chartbeat.com>
2015-03-21 14:22:04 -0400

    comment out unused routes

--- server.js
+++ server.js
@@ -13,23 +13,23 @@ app.set('view engine', 'handlebars');
 // Index Route
 app.get('/', router.index);
 
-// Sign Up Route
-app.get('/sign-up', router.sign_up);
+// // Sign Up Route
+// app.get('/sign-up', router.sign_up);
 
-// Public Profile Route
-app.get('/:username', router.profile_public);
+// // Public Profile Route
+// app.get('/:username', router.profile_public);
 
-// Private Profile Route
-app.get('/:username/profile', router.profile_private);
+// // Private Profile Route
+// app.get('/:username/profile', router.profile_private);
 
-// Messages Route
-app.get('/:username/messages', router.messages);
+// // Messages Route
+// app.get('/:username/messages', router.messages);
 
-// Friends List Route
-app.get('/:username/friends', router.friends);
+// // Friends List Route
+// app.get('/:username/friends', router.friends);
 
-// Map Route
-app.get('/finder', router.finder);
+// // Map Route
+// app.get('/finder', router.finder);
 
 // Set /public as our static content dir
 app.use('/', express.static(__dirname + '/public/'));
