5692a4c700615dc9ecf27020dbdcb95c8e3467c1
Jenn Creighton <jenn@chartbeat.com>
2015-03-22 19:59:48 -0400

    favicon & character count for suggestion box

--- components/inputs/app-textbox.js
+++ components/inputs/app-textbox.js
@@ -8,14 +8,15 @@ var TextBox = React.createClass({
   getInitialState: function() {
     return {
       isVisible: false,
-      isValid: false
+      isValid: false,
+      characterCount: ''
     }
   },
   getDefaultProps: function() {
     return {
       label: 'Suggestions? We\'d Love to Hear Them!',
       placeholder: 'Would love to hear your thoughts!',
-      errorMessage: 'Must be under 300 characters!'
+      errorMessage: 'Must be under 600 characters!'
     }
   },
   propTypes: {
@@ -23,11 +24,16 @@ var TextBox = React.createClass({
     placeholder: React.PropTypes.string,
     errorMessage: React.PropTypes.string
   },
+  updateCharacterCount: function() {
+    var length = this.refs.text.getDOMNode().value.length;
+
+    this.setState({characterCount: (600 - length)});
+  },
   isValid: function() {
     // check that text is below 300 characters
     var value = this.refs.text.getDOMNode().value;
 
-    var isValidText = (value.length < 300);
+    var isValidText = (value.length < 600);
 
     if (!isValidText) {
       this.setState({isVisible: true, isValid: false});
@@ -38,10 +44,17 @@ var TextBox = React.createClass({
     return {text: isValidText && value};
   },
   render: function() {
+    var cx = React.addons.classSet;
+    var classes = cx({
+      'character-counter': true,
+      'warning': (this.state.characterCount < 10),
+      'hidden': (this.state.characterCount == 0)
+    });
     return (
       <div className="ob-input textbox">
         <label>{this.props.label}</label>
-        <textarea ref="text" onBlur={this.isValid}></textarea>
+        <textarea ref="text" onChange={this.updateCharacterCount} onBlur={this.isValid}></textarea>
+        <span className={classes}>{this.state.characterCount}</span>
         <Error isVisible={this.state.isVisible} errorMessage={this.props.errorMessage} />
       </div>
     );
